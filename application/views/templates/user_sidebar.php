8c659730fefa74fab07e295829f47982e9721c47
plantoge <arsipfauzi@gmail.com>
2019-11-16 12:27:19 +0700

    6menuManagement

--- application/views/templates/user_sidebar.php
+++ application/views/templates/user_sidebar.php
@@ -10,31 +10,54 @@
 </a>
 
 <!-- Divider -->
-<hr class="sidebar-divider my-0">
-
-<!-- Nav Item - Dashboard -->
-<li class="nav-item">
-  <a class="nav-link" href="index.html">
-    <i class="fas fa-fw fa-tachometer-alt"></i>
-    <span>Dashboard</span></a>
-</li>
-
-<!-- Divider -->
 <hr class="sidebar-divider">
 
+<!-- QUERY MENU -->
+<?php 
+  $role_id = $this->session->userdata('role_id');
+  $queryMenu = "
+      SELECT user_access_menu.id, menu
+      FROM user_access_menu JOIN user_menu
+      ON user_access_menu.menu_id = user_menu.id
+      WHERE user_access_menu.role_id = $role_id
+      ORDER BY user_access_menu.menu_id ASC
+  ";
+
+  $menu = $this->db->query($queryMenu)->result_array();
+  // var_dump($menu);
+?>
+
+
 <!-- Heading -->
-<div class="sidebar-heading">
-  User
-</div>
+<?php foreach ($menu as $m):?>
+  <div class="sidebar-heading">
+    <?= $m['menu']; ?>
+  </div>
 
-<li class="nav-item">
-      <a class="nav-link" href="charts.html">
-      <i class="fas fa-id-card-alt"></i>
-    <span>My Profile</span></a>
-</li>
+    <?php 
+      $menuId = $m['id'];
+      $querySubMenu = "
+          SELECT * FROM user_sub_menu JOIN user_menu
+          ON user_sub_menu.menu_id = user_menu.id 
+          WHERE user_sub_menu.menu_id = $menuId
+          AND user_sub_menu.is_active = 1
+      "; 
 
-<!-- Divider -->
-<hr class="sidebar-divider d-none d-md-block">
+      $subMenu = $this->db->query($querySubMenu)->result_array();
+    ?>
+    
+    <?php foreach ($subMenu as $sm) : ?>
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item">
+          <a class="nav-link" href="<?= base_url($sm['url']); ?>">
+            <i class="<?= $sm['icon']; ?>"></i>
+            <span><?= $sm['title'] ?></span></a>
+        </li>
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+    <?php endforeach; ?>
+
+<?php endforeach; ?>
 
 <!-- Sidebar Toggler (Sidebar) -->
 <div class="text-center d-none d-md-inline">
